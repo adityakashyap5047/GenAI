9df7352c3125c00311450ed744ec92d3bfb6fd94
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-07 20:15:25 +0530

    creating the app for the Ollama chatbot

--- 4. Chatbot/main.py
+++ 4. Chatbot/main.py
@@ -1,6 +1,6 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.llms import ollama
+from langchain_community.llms import Ollama
 
 import streamlit as st
 import os
@@ -11,7 +11,7 @@ load_dotenv()
 ### Langchain Tracking
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+os.environ["LANGCHAIN_PROJECT"] = "GENAI - APP With Ollama"
 
 ### Prompt Template
 prompt = ChatPromptTemplate.from_messages(
@@ -22,8 +22,22 @@ prompt = ChatPromptTemplate.from_messages(
 )
 
 def generate_response(question, llm = "gemma:2b"):
-    llm = ollama(model=llm)
+    llm = Ollama(model=llm)
     parser = StrOutputParser()
     chain = prompt | llm | parser
     answer = chain.invoke({"question": question})
     return answer
+
+
+### Title of the app
+st.title("Enhanced Q&A Chatbot")
+
+### Main interface for user input
+st.write("## Ask your question")
+question = st.text_input("Enter your question here")
+
+if question:
+    response = generate_response(question)
+    st.write(response)
+else:
+    st.write("Please enter a question to get a response.")
