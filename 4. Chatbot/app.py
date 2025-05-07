f52b4619a15d9fade893bd144a1d4bcfbc33bb70
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-07 19:28:53 +0530

    creating the prompt template for Q&A Chatbot

---
+++ 4. Chatbot/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import openai
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+### Langchain Tracking
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+
+### Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please response to the user queries"),
+        ("user", "Question: {question}"),
+    ]
+)
+
