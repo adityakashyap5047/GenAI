b1b21f65e54ac822ca034206bec8782f22d89e91
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-07 20:23:16 +0530

    importing all the dependencies for RAG Q&A Chatbot

---
+++ 5. RAG Q&A Chatbot/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import os
+
+from langchain_groq import ChatGroq
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS  
+from langchain_community.document_loaders import PypdfLoader
+
