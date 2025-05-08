e6bc063330e142bc42724edff6d3a8563858b56d
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-08 17:23:14 +0530

    creating the vector embedding for the entire work flow

--- 5. RAG Q&A Chatbot/app.py
+++ 5. RAG Q&A Chatbot/app.py
@@ -8,7 +8,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS  
-from langchain_community.document_loaders import PypdfLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -32,4 +32,13 @@ prompt = ChatPromptTemplate.from_template(
 
     """
 )
+
+def create_vector_embedding():
+    if "vecotrs" not in st.session_state:
+        st.session_state.embeddings = OllamaEmbeddings(model="gemma:2b")
+        st.session_state.loader = PyPDFDirectoryLoader("Research Paper")
+        st.session_state.docs = st.session_state.loader.load()
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        st.session_state.split_docs = st.session_state.text_splitter.split_documents(st.session_state.docs[: 50])
+        st.session_state.vectorstore = FAISS.from_documents(st.session_state.split_docs, st.session_state.embeddings)
 
