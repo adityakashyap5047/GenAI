8ae62bd8926c7420b6175686ad28193d4b28c908
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-09 17:09:10 +0530

    create the prompt template for the qa prompt

--- 6. RAG Q&A Conversation/app.py
+++ 6. RAG Q&A Conversation/app.py
@@ -76,3 +76,24 @@ if groq_api_key:
     )
 
     history_aware_retriever = create_history_aware_retriever(llm, retriever, contextualize_q_prompt)
+
+    ### Answer question
+    system_prompt = (
+        "You are an assistant for question-answering tasks."
+        "Use the following pieces of retrieved context to answer"
+        "the question. If you don't know the answer, say that you"
+        "don't know. Use three sentences maximum and keep the"
+        "answer concise."
+        "\n\n"
+        "{context}"
+    )
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    
