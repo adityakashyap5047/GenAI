c2f6b0a2bf5e06327d4a5cff8facd859c04bbeee
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-10 20:54:05 +0530

    creating the table and inserting the records into it

--- 8. Chat SQL/sqlite.py
+++ 8. Chat SQL/sqlite.py
@@ -6,3 +6,21 @@ connection = sqlite3.connect('student.db')
 ### Create a cursor object to interact with the database
 cursor = connection.cursor()
 
+### Create a table
+table_info = """
+
+    create table STUDENT(NAME VARCHAR(25), CLASS VARCHAR(25),
+                         SECTION VARCHAR(25), MARKS INT)
+
+"""
+
+cursor.execute(table_info)
+
+### Insert data into the table
+cursor.execute('''Insert Into STUDENT values('Krish', 'Data Science', 'A', 90)''')
+cursor.execute('''Insert Into STUDENT values('John', 'Data Science', 'B', 100)''')
+cursor.execute('''Insert Into STUDENT values('Sam', 'Data Science', 'C', 95)''')
+cursor.execute('''Insert Into STUDENT values('Tom', 'DEVOPS', 'A', 85)''')
+cursor.execute('''Insert Into STUDENT values('Jerry', 'DEVOPS', 'B', 80)''')
+
+
