a7e1a4c59d4b33e28595be59d6bd73fc947ff72e
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-06-23 10:43:27 +0530

    create the basic structure of the chat app

--- 8. Chat SQL/app.py
+++ 8. Chat SQL/app.py
@@ -9,3 +9,30 @@ from sqlalchemy import create_engine
 import sqlite3
 from langchain_groq import ChatGroq
 
+st.set_page_config(page_title="LangChain: Chat with SQL DB", page_icon="🦜")
+st.title("🦜 LangChain: Chat with SQL DB")
+
+LOCALDB = 'USE_LOCAL_DB'
+MYSQL = 'USE_MYSQL_DB'
+
+radio_opt = ["Use SQLITE 3 DATABASE - STUDENT.db", "Connect to your own SQL Database"]
+
+selected_opt = st.radio(label="Choose a DB which you want to chat", options=radio_opt)
+
+if radio_opt.index(selected_opt) == 1:
+    db_uri = MYSQL
+    mysql_host = st.sidebar.text_input("Provide MySQL Host")
+    mysql_user = st.sidebar.text_input("Provide MySQL User")
+    mysql_password = st.sidebar.text_input("Provide MySQL Password", type="password")
+    mysql_db = st.sidebar.text_input("Provide MySQL Database Name")
+else:
+    db_uri = LOCALDB
+
+api_key = st.sidebar.text_input("Enter your Groq API Key", type="password")
+
+if not db_uri:
+    st.info("Please enter the database information and uri")
+
+if not api_key:
+    st.info("Please enter your Groq API Key")
+
