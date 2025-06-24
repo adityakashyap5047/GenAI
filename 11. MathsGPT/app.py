b9a9301701c18f70215d57dc43fb721dbc1c10b5
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-06-24 17:10:17 +0530

    starting with installing the dependencies and model

---
+++ 11. MathsGPT/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain.chains import LLMChain, LLMMathChain
+from langchain.prompts import PromptTemplate
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.agents.agent_types import AgentType
+from langchain.agents import Tool, initialize_agent
+from langchain.callbacks import StreamlitCallbackHandler
+
+### Set up the streamlit app
+st.set_page_config(page_title="MathsGPT", page_icon="🦜")
+st.title("🦜 MathsGPT: Your Personal Math Assistant")
+
+groq_api_key = st.sidebar.text_input("Enter your Groq API Key", type="password")
+
+if not groq_api_key:
+    st.warning("Please enter your Groq API Key to use the app.")
+    st.stop()
+
+llm = ChatGroq(model="Gemma2-9b-It", groq_api_key=groq_api_key)
+
