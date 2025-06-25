53efd304f1d24cb138a85d484c512777e9dfdb1a
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-06-25 23:12:32 +0530

    Create a senior blog writer with YT tool

--- 16. CrewAI Agents/agents.py
+++ 16. CrewAI Agents/agents.py
@@ -11,5 +11,20 @@ blog_researcher = Agent(
         "Expert in understanding videos in AI Data Science, Machine Learning and Gen AI and providing suggestion"
     ),
     tools=[],
-    allow_delegation=True
+    allow_delegation=True ### Pass to share the task to other agents
+)
+
+### Create a senior blog writer with YT tool
+blog_writer = Agent(
+    role='Blog Writer',
+    goal='Narrate compelling tech stories about the video {topic} from YT channels',
+    verbose=True,
+    memory=True,
+    backstory=(
+        "With a flair for simplifying complex topics, you craft"
+        "engaging narratives that captivate and educate, bringing new"
+        "discoveries to light in an acessible manner."
+    ),
+    tools=[],
+    allow_delegation=False
 )
